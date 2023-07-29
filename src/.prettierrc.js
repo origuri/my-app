5f35b5178e0e9a4fc3234a83f2d9a82fa2f8f5c2
origuri <dlcksrb7@naver.com>
2023-07-29 16:38:24 +0900

    2023.7.29 : 리액트 설정 완료

---
+++ src/.prettierrc.js
@@ -0,0 +1,7 @@
+module.exports =  {
+    semi:  true,    // 내가 안붙여도 세미콜론이 붙음
+    trailingComma:  'all', // 코드 끝에 자동으로 , 를 넣어준다.
+    singleQuote:  true, // 저장시에 '' 로 변경된다
+    printWidth:  80,
+    tabWidth:  2, // tab키 누르면 2칸 띄워짐
+};
