f5fc837d395dd258646919c17a9618207f3b1901
origuri <dlcksrb7@naver.com>
2023-07-29 19:18:14 +0900

    2023.7.29 : 리액트 기본 문법, 불변 함수(spread, concat, filter, slice, map), 불변 함수 응용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+/*
+ * 1. return 시에 하나의 dom만 리턴할 수 있다.
+ * 2. 변수 선언은 let 또는 const로만 해야 함.
+ * 3. if 사용 불가능 -> 삼항연산자를 사용 가능(조건 ? 값(true) : 값(false))
+ * 4. 조건부 렌더링 -> &&
+ * 5. 리엑트에선 선언만 하면 null이 아닌 undifined가 선언 됨.
+ * 6. css 디자인
+ *      - 내부에 적는 방법
+ *      - 외부 파일에 적는 방법
+ *      - 라이브러리를 사용하는 방법(부트스트랩, compent-style 등)
+ * */
+
+let a = 10; // 변수
+const b = 20; // 상수
 
 function App() {
-  return <div>안녕1ssds</div>;
+  let c;
+  /* 리엑트에선 선언만 하면 null이 아닌 undifined가 선언 됨.*/
+  console.log(1, c);
+  /* 비추 */
+  const myStyle = {
+    color: 'red',
+  };
+
+  let list = [1, 2, 3];
+  return (
+    <div>
+      안녕1ssds
+      {/*{ }를 이용해서 변수를 사용할 수 있음.*/}
+      <div style={myStyle}>ss{a === 10 ? '10입니다.' : '10이 아닙니다.'}</div>
+      {/* &&를 사용하면 특정 조건에 부합할 때만 렌더링 됨.  */}
+      <h1 className="box-style">dd{b === 20 && '20입니다'}</h1>
+      <div>
+        {list.map((n) => (
+          <h1>{n + 10}</h1>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App /> {/*jsx 문법*/}
+    <App />
   </React.StrictMode>,
 );
 
