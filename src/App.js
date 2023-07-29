4db9ade4a8db6a61e176626ab3f0abf3965fb9b5
origuri <dlcksrb7@naver.com>
2023-07-29 22:35:54 +0900

    2023.7.29 - useEffect와 사용법

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react'; // { }가 있다는 건 defalut export가 아니라는 것.
 import Sub from './Sub';
+import { cleanup } from '@testing-library/react';
+
 /*
  * 1. return 시에 하나의 dom만 리턴할 수 있다.
  * 2. 변수 선언은 let 또는 const로만 해야 함.
@@ -15,47 +17,54 @@ import Sub from './Sub';
  * */
 
 function App() {
-  let list = [1, 2, 3];
-  console.log('app 실행됨.');
-  // let number = 1; 상태 값이 아님.
-  const [number, setNumber] = useState(1); // react의 hooks 라이브러리 사용
-  const add = () => {
-    /*
-     * setNumber로만 값을 변경할 수 있음
-     * number++; 상수 선언을 했기 때문에 값을 직접적으로 변경할 수 없음.
-     * 리엑트한테 number 값 변경할게 라고 요청하는 것.
-     * */
-    setNumber(number + 1);
-    console.log('add', number);
-  };
-  let sample = [
-    { id: 1, name: '홍일' },
-    { id: 2, name: '홍이' },
-    { id: 3, name: '홍삼' },
-    { id: 4, name: '홍사' },
-  ];
+  const [data, setData] = useState(0);
+  const [search, setSearch] = useState(0);
 
-  const [users, setUsers] = useState(sample);
-  /*
-   * 기존의 user 객체에 있는 데이터와 값은 동일하지만
-   * user 객체와 sample 객체는 서로 다른 객체이므로
-   * 같은 값의 데이터할지라도 객체가 다르기 때문에 새로 랜더링이 된다.
-   * ...sample은 최초 실행때는 객체가 달라서 리렌더링이 되지만
-   * 한번 하고 나면 ...sample은 4개이므로 리렌더링되지만 값은 변하지 않음.
-   * */
   const download = () => {
-    setUsers([...sample, { id: 5, name: 'aaa' }]);
+    // 다운로드 받은 데이터가 5라는 가정
+    let downloadData = 5;
+    setData(downloadData);
   };
-  // 상태 값이 변해야 랜더링이 실행 됨.
+
+  /*
+   * 실행시점 :
+   * 1. App() 함수가 최초 실행 될 때(최초로 그림이 그려질 때)
+   * 2. 상태 변수가 변경 될 때(dependecyList에 등록이 되어있어야 함. )
+   *
+   * useEffect의 두번째 파라미터가 디펜던시 리스트인데 [] 공백으로 두면
+   * 어떤 상태 변수에도 의존하지 않아서 최초 app이 실행될 때에만 실행이 됨.
+   *
+   * 사용 용도 :
+   * 게시판 리스트가 있다고 칠 때 처음 실행하자마자 통신으로
+   * 게시판 리스트를 싹 받아와서 뿌릴 수 있음.
+   *
+   * 특정 게시판을 찾는 검색 함수(search)가 있을 때 그림을 다시 그려야 함.
+   * 그러므로 두번째 파라미터에는 [search]가 있어야지 검색이 되지 빈 배열 []이 있으면 검색안됨.
+   * */
+  useEffect(() => {
+    console.log('useEffect 실행됨.');
+    download();
+  }, [search]);
+
+  const aa = () => {
+    // input의 값이 바뀌어야 실행 됨.
+    // 1 검색 후 2 검색 하면 상태가 변해서 실행이 되지만 1 검색 후 1 검색하면 상태가 안변해서 실행 안됨.
+    const sea = document.querySelector('#search').value;
+    setSearch(sea);
+  };
+
   return (
     <div>
-      <h1>숫자 : {number}</h1>
-      <button onClick={add}>더하기</button>
-      <button onClick={download}>다운로드</button>
-      {users.map((u) => (
-        <h1>{(u.id, u.name)}</h1>
-      ))}
-      <Sub />
+      <input type="text" id={'search'}></input>
+      <button onClick={aa}>검색</button>
+      <h1>데이터 : {data}</h1>
+      <button
+        onClick={() => {
+          setData(data + 1);
+        }}
+      >
+        더하기
+      </button>
     </div>
   );
 }
